1c4efa87f779eb1bad9142f95ae372ff1f8d6c1b
Minsu Kim <alstn9008@naver.com>
2020-10-14 06:21:33 +0900

    Final Test

--- MyApp.java
+++ MyApp.java
@@ -12,23 +12,23 @@ public class MyApp
         try
         {
             File inputFile = new File("C:\\Users\\Michael Kim\\Desktop\\2020-2\\ADP-2\\실습#3\\sunmoon.jpg");
-            FileReader fr = new FileReader(inputFile);
-            BufferedReader br = new BufferedReader(fr, 20);
+            FileInputStream fis = new FileInputStream(inputFile);
+            BufferedInputStream bis = new BufferedInputStream(fis, 20);
             
             File outputFile = new File("C:\\Users\\Michael Kim\\Desktop\\2020-2\\ADP-2\\실습#3\\Result\\sunmoon-copy.jpg");
-            FileWriter fw = new FileWriter(outputFile);
-            BufferedOutputStream bos = new BufferedOutputStream(fw, 20);
+            FileOutputStream fos= new FileOutputStream(outputFile);
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 20);
 
             int c;  
-            while((c = fr.read()) != -1)
+            while((c = fis.read()) != -1)
             {
                 bos.write((byte)c);
             }
-            fr.close();
+            fis.close();
             bos.flush();
             bos.close();     
-            System.out.println("입력파일 : " + inputFile.getPath() + "(파일크기 = " + inputFile.getTotalSpace() + "바이트)");
-            System.out.println("입력파일 : " + outputFile.getPath() + "(파일크기 = " + outputFile.getTotalSpace() + "바이트)");
+            System.out.println("입력파일 : " + inputFile.getPath() + "(파일크기 = " + inputFile.length() + "바이트)");
+            System.out.println("입력파일 : " + outputFile.getPath() + "(파일크기 = " + outputFile.length() + "바이트)");
         }
         catch(IOException e)
         {
